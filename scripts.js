d10677c23b47fff48d95f77ea3fa67bbcdb7a1c3
Tarun Kumar Dey <111647518+Tarun123a@users.noreply.github.com>
2022-11-05 02:18:48 +0530

    Tarun

---
+++ scripts.js
@@ -0,0 +1,33 @@
+function addMore(){
+    document.getElementById('error').innerHTML="";
+
+    let name=document.getElementById('name').value;
+    if(name==''){
+        document.getElementById('error').innerHTML="Please Enter Value";
+    }
+    else{
+        let box=document.getElementById('box');
+        let li=document.createElement('li');
+        li.textContent=name;
+
+        let a=document.createElement('a');
+        a.textContent="x";
+        a.href="javascript:void(0)";
+        a.className="remove";
+        li.appendChild(a);
+
+        let pos=box.firstElementChild;
+        if(pos==null){
+            box.appendChild(li);
+        }
+        box.insertBefore(li,pos);
+    }
+    document.getElementById('name').value="";
+}
+ 
+let btn=document.querySelector('ul');
+btn.addEventListener('click',function(e){
+    let box=document.getElementById('box');
+    let li=e.target.parentNode;
+    box.removeChild(li);
+});
